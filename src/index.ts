88499d2b213e374676d7d0f5940f44eb0a5b29b0
Mukund Maheshwari <maheshwari31mukund@gmail.com>
2025-06-20 19:24:03 +0530

    global on all post

--- src/index.ts
+++ src/index.ts
@@ -33,6 +33,6 @@ app.get('/', async (req, res) => {
 
 app.use(errorHandler)
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT as number, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT} (bound to 0.0.0.0)`);
 });
