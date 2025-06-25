56fe36a2656d6185f2cee2ad12301a8cf47095e4
Mukund Maheshwari <maheshwari31mukund@gmail.com>
2025-06-25 10:25:38 +0530

    updated cors

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+// app.use(cors());
 
 app.use("/api/v1", authRouter);
 app.use("/api/v1", companyRouter);
